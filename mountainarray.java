90b12497ba1cc716f4407f3dcc83e5d712d6fa94
MTalhaofc <talha0750@gmail.com>
2024-08-24 19:40:40 +0500

    Aug 24

---
+++ mountainarray.java
@@ -0,0 +1,76 @@
+public class mountainarray {
+
+    public static void main(String[] args) {
+        
+    }
+    int search(int[]arr, int target){
+int peak = peakIndexInMountainArray(arr);
+int firsttry = binarysearch(arr, target, 0, peak);
+if(firsttry!= -1){
+    return firsttry;
+}else{
+    return binarysearch(arr, target, peak +1, arr.length-1);
+}
+
+
+    }
+public int peakIndexInMountainArray(int[] arr){
+
+    int start = 0;
+    int end =arr.length -1;
+    while (start <=end) {
+        int mid = start + (end - start)/2;
+        if(arr[mid]> arr[mid+1]){
+            end = mid;
+        }
+        else{
+            start = mid +1 ;
+        }
+    }
+    return start;
+}
+
+static int binarysearch(int[] arr, int target, int start, int end){
+
+    
+    
+boolean isAsc = arr[start] < arr[end];
+
+
+    while (start<=end){
+        // int mid = (start + end)/2; // might be possible exceeds range of int
+    int mid = start + (end-start) / 2;
+    
+if(arr[mid] == target){
+return mid;
+}
+if(isAsc){
+if(target < arr[mid]){
+    end=mid-1;
+    
+    }
+    else {
+        start = mid+1;
+    
+    
+    }   
+}
+else{
+if(target > arr[mid]){
+    end=mid-1;
+    
+    }
+    else {
+        start = mid+1;
+    
+    
+    } 
+}
+    
+    
+    }
+    
+    return arr[start];
+
+}
+}
